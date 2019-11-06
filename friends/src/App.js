7bcd910e9f3bfb89ba936742a1b97891b4db985c
Danielle B <danibaxx@hotmail.com>
2019-11-05 19:26:17 -0700

    added components folder with FriendsList.js and Login.js, added routes and links to App.js for components

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Route, Link, withRouter } from 'react-router-dom';
+
+// components
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav>
+        <Link to='/login'>Login</Link>
+        <Link to='/account'>Account</Link>
+      </nav>
 
-      </header>
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/account' component={FriendsList} />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
